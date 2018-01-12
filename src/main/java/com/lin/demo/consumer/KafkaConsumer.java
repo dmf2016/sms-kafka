0e76a18dffde7ae288e0184e89577124a30e0316
dmf2016 <dengmingfeng@163.com>
2018-01-12 16:19:14 +0800

    d

--- src/main/java/com/lin/demo/consumer/KafkaConsumer.java
+++ src/main/java/com/lin/demo/consumer/KafkaConsumer.java
@@ -14,49 +14,48 @@ import kafka.utils.VerifiableProperties;
 
 import com.lin.demo.producer.KafkaProducer;
 
+/**
+ * 消费者
+ * @Description
+ * @Project: sms-kafka
+ * @Date:Jan 12, 2018
+ * @Author dmf
+ * @Copyright (c) 2018, 33e9 All Rights Reserved.
+ */
 public class KafkaConsumer {
-
 	private final ConsumerConnector consumer;
 
 	private KafkaConsumer() {
 		Properties props = new Properties();
 		// zookeeper 配置
-		props.put("zookeeper.connect", "127.0.0.1:2181");
-
+		props.put("zookeeper.connect", "10.0.30.151:2181");
 		// group 代表一个消费组
-		props.put("group.id", "linlin-group");
-
+		props.put("group.id", "0");
 		// zk连接超时
 		props.put("zookeeper.session.timeout.ms", "4000");
 		props.put("zookeeper.sync.time.ms", "200");
 		props.put("rebalance.max.retries", "5");
 		props.put("rebalance.backoff.ms", "1200");
-		
-	
 		props.put("auto.commit.interval.ms", "1000");
 		props.put("auto.offset.reset", "smallest");
 		// 序列化类
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
-
 		ConsumerConfig config = new ConsumerConfig(props);
-
 		consumer = kafka.consumer.Consumer.createJavaConsumerConnector(config);
 	}
-
 	void consume() {
 		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
 		topicCountMap.put(KafkaProducer.TOPIC, new Integer(1));
-
 		StringDecoder keyDecoder = new StringDecoder(new VerifiableProperties());
 		StringDecoder valueDecoder = new StringDecoder(new VerifiableProperties());
-
-		Map<String, List<KafkaStream<String, String>>> consumerMap = consumer.createMessageStreams(topicCountMap, keyDecoder, valueDecoder);
+		Map<String, List<KafkaStream<String, String>>> consumerMap = consumer.createMessageStreams(topicCountMap,
+				keyDecoder, valueDecoder);
 		KafkaStream<String, String> stream = consumerMap.get(KafkaProducer.TOPIC).get(0);
 		ConsumerIterator<String, String> it = stream.iterator();
 		while (it.hasNext())
-			System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" + it.next().message() + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+			System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" + it.next().message()
+					+ "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 	}
-
 	public static void main(String[] args) {
 		new KafkaConsumer().consume();
 	}
--- src/main/java/com/lin/demo/producer/KafkaProducer.java
+++ src/main/java/com/lin/demo/producer/KafkaProducer.java
@@ -6,39 +6,42 @@ import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
+/**
+ * 生产者
+ * @Description
+ * @Project: sms-kafka
+ * @Date:Jan 12, 2018
+ * @Author dmf
+ * @Copyright (c) 2018, 33e9 All Rights Reserved.
+ */
 public class KafkaProducer {
 	private final Producer<String, String> producer;
-	public final static String TOPIC = "linlin";
+	public final static String TOPIC = "test";
 
 	private KafkaProducer() {
 		Properties props = new Properties();
 		// 此处配置的是kafka的端口
-		props.put("metadata.broker.list", "127.0.0.1:9092");
-		props.put("zk.connect", "127.0.0.1:2181");  
-
+		props.put("metadata.broker.list", "10.0.30.151:9092");
+		props.put("zk.connect", "10.0.30.151:2181");
 		// 配置value的序列化类
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
 		// 配置key的序列化类
 		props.put("key.serializer.class", "kafka.serializer.StringEncoder");
-
 		props.put("request.required.acks", "-1");
-
 		producer = new Producer<String, String>(new ProducerConfig(props));
 	}
-
 	void produce() {
 		int messageNo = 1000;
 		final int COUNT = 10000;
-
 		while (messageNo < COUNT) {
 			String key = String.valueOf(messageNo);
-			String data = "INFO JobScheduler: Finished job streaming job 1493090727000 ms.0 from job set of time 1493090727000 ms" + key;
+			String data = "INFO JobScheduler: Finished job streaming job 1493090727000 ms.0 from job set of time 1493090727000 ms"
+					+ key;
 			producer.send(new KeyedMessage<String, String>(TOPIC, key, data));
 			System.out.println(data);
 			messageNo++;
 		}
 	}
-
 	public static void main(String[] args) {
 		new KafkaProducer().produce();
 	}
